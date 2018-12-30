ebe60495385bf6bc87a1996e8103a255bdd84e14
Spened <cm.pwnedstudios@gmail.com>
2018-12-30 21:21:48 +0100

    Start project

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Noyaret Louis</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Présentation</a></li>
+          <li><a href="#">Parcoure</a></li>
+          <li><a href="#">Projet</a></li>
+          <li><a href="#">Veille Technologique</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row presentation">
+        <div class="col-sm-12">
+          <h2>Présentation</h2>
+          <h4>Me concernant :</h4>
+          <p>Jeune homme dynamique et motivé, l'envie de découvrire de nouveau horizon et le file rouge de ma vie.</p>
+          <p>Voir, apprendre, maîtrisé sont des choses importante pour moi !</p>
+          <p>Je suis étudiant en BTS SIO à Ella Fitzgerald et à la fois Directeur Général de mon entreprise de développement d'application sur téléphone.</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
