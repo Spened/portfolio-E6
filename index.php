987fae9c1e9f98b2f7520cc080d70d16c2c6cc75
Spened <cm.pwnedstudios@gmail.com>
2019-01-02 01:03:03 +0100

    Ajout présentation, parcours, ancrage

--- index.php
+++ index.php
@@ -10,21 +10,53 @@
     <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Présentation</a></li>
-          <li><a href="#">Parcoure</a></li>
-          <li><a href="#">Projet</a></li>
-          <li><a href="#">Veille Technologique</a></li>
+          <li class="active"><a href="#presentation">Présentation</a></li>
+          <li><a href="#parcours">Parcours</a></li>
+          <li><a href="#projet">Projet</a></li>
+          <li><a href="#techno">Veille Technologique</a></li>
         </ul>
       </div>
     </nav>
     <div class="container">
-      <div class="row presentation">
+      <div class="row">
+        <div class="col-sm-12 introduction text-center">
+          <img class="img-circle" src="img/pdp.jpg" alt="pdp" height="150" width="150">
+          <h2>NOYARET Louis</h2>
+        </div>
+      </div>
+      <hr>
+      <div id="presentation" class="row">
         <div class="col-sm-12">
           <h2>Présentation</h2>
           <h4>Me concernant :</h4>
           <p>Jeune homme dynamique et motivé, l'envie de découvrire de nouveau horizon et le file rouge de ma vie.</p>
           <p>Voir, apprendre, maîtrisé sont des choses importante pour moi !</p>
           <p>Je suis étudiant en BTS SIO à Ella Fitzgerald et à la fois Directeur Général de mon entreprise de développement d'application sur téléphone.</p>
+        </div>
+      </div>
+      <hr>
+      <div id="parcours" class="row">
+        <div class="col-sm-12 text-center">
+          <h2>Parcours</h2>
+          <h4>Scolaire</h4>
+        </div>
+        <div class="col-sm-4"></div>
+        <div class="col-sm-2 text-center">
+          <p></p>
+          <img class="img-circle" src="img/elec.png" alt="lycée" height="75" width="75">
+          <p>BAC PRO ELEEC</p>
+          <p>MENTION BIEN</p>
+        </div>
+        <div class="col-sm-2 text-center">
+          <img class="img-circle" src="img/pc.jpg" alt="bts" height="75" width="75">
+          <p>BTS SIO [SLAM]</p>
+        </div>
+        <div class="col-sm-4"></div>
+      </div>
+      <hr class="parcours-hr">
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <h4>Projet</h4>
         </div>
       </div>
     </div>
